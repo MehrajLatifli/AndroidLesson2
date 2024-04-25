4a3593803e5f649ccbcf0218b7569228a97b73cc
MehrajLatifli <meracletifli@gmail.com>
2024-04-25 04:09:49 +0400

    GOD bless universes

--- app/src/main/java/com/example/androidlesson2/MainActivity.kt
+++ app/src/main/java/com/example/androidlesson2/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.androidlesson2
 
+import android.content.res.ColorStateList
 import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
@@ -7,8 +8,14 @@ import android.widget.RadioButton
 import android.widget.RadioGroup
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.forEach
 import com.example.androidlesson2.databinding.ActivityMainBinding
+import com.google.android.material.switchmaterial.SwitchMaterial
 import kotlin.random.Random
+import com.google.android.material.materialswitch.MaterialSwitch
+import androidx.appcompat.widget.AppCompatRadioButton
+
+
 
 fun randomRange(from: Int? = null, until: Int? = null): Int {
     from.let {
@@ -19,6 +26,45 @@ fun randomRange(from: Int? = null, until: Int? = null): Int {
         }
     }
 }
+
+
+fun setSwitchAppearance(switch: MaterialSwitch, isChecked: Boolean, binding: ActivityMainBinding) {
+
+    if (isChecked) {
+
+        switch.thumbTintList = ColorStateList.valueOf(Color.parseColor("#4CAF50"))
+        switch.trackTintList = ColorStateList.valueOf(Color.parseColor("#FFDD00"))
+        switch.text = "Light"
+        switch.trackDrawable?.setTint(Color.parseColor("#FFDD00"))
+
+        switch.setTextColor(Color.BLACK)
+
+        binding.radioGroup.forEach { view ->
+            if (view is RadioButton) {
+                view.setTextColor(Color.BLACK)
+
+            }
+        }
+
+    } else {
+
+        switch.thumbTintList = ColorStateList.valueOf(Color.parseColor("#F8F8F8"))
+        switch.trackTintList = ColorStateList.valueOf(Color.parseColor("#000000"))
+        switch.trackDrawable?.setTint(Color.parseColor("#FFC0CB"))
+        switch.text = "Dark"
+
+        switch.setTextColor(Color.WHITE)
+
+        binding.radioGroup.forEach { view ->
+            if (view is RadioButton) {
+                view.setTextColor(Color.WHITE)
+            }
+        }
+
+    }
+}
+
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -67,5 +113,26 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
+
+
+
+        binding.switch1.setOnCheckedChangeListener { buttonView, isChecked ->
+
+            setSwitchAppearance(binding.switch1, isChecked,binding)
+
+            if (isChecked) {
+
+                binding.root.setBackgroundColor(Color.parseColor("#FFFFFF"))
+
+            } else {
+
+                binding.root.setBackgroundColor(Color.parseColor("#333333"))
+            }
+
+        }
+
+
+
+
     }
 }
